6a231a5002335cbac5370f79a79f11b00ff25949
SinghAman21 <singhaman1825@gmail.com>
2025-12-17 16:55:02 +0530

    made it center and sw.js

--- components/QuoteBlock.tsx
+++ components/QuoteBlock.tsx
@@ -11,7 +11,7 @@ export default function QuoteBlock({ quote, animationDelay = '0.05s' }: QuoteBlo
       className="mt-12 animate-[slideFadeUp_0.25s_ease-out] mb-12"
       style={{ animationDelay, animationFillMode: 'both' }}
     >
-      <blockquote className="relative italic text-gray-700 dark:text-neutral-400 py-4 px-6 max-w-2xl">
+      <blockquote className="relative italic text-gray-700 dark:text-neutral-400 py-4 px-6 max-w-2xl mx-auto">
         <CornerMarkers variant="static" />
         <p className="leading-relaxed text-center">
           {quote}
