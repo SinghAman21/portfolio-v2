518eb2d3dadecafce25996fdfa61fd13a9b2ecce
SinghAman21 <singhaman1825@gmail.com>
2026-03-07 17:39:48 +0530

    testing wa and new resume pdf

--- app/layout.tsx
+++ app/layout.tsx
@@ -192,6 +192,11 @@ export default function RootLayout({
             }),
           }}
         />
+          <script 
+          data-site-hex="wj9es3ceo4i5" 
+          src="https://wa-be.vercel.app/ultrafree.js"
+          async
+        />
       </head>
       <body
         className={cn(
